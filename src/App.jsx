727c6f5a1f5e8ef9621ed693076b64ac6b29ddee
Nirmal Jaiswal <nirmaljaiswal7746@gmail.com>
2023-04-07 01:42:32 +0530

    use state basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import Final from './tutorial/01-useState/final/01-error-example';
 
 function App() {
-  return <h1>Advanced React</h1>;
+  return (
+    <div className='container'>
+      <Final />
+    </div>
+  );
 }
 
 export default App;
--- src/tutorial/01-useState/final/01-error-example.jsx
+++ src/tutorial/01-useState/final/01-error-example.jsx
@@ -1,17 +1,17 @@
 const ErrorExample = () => {
   let count = 0;
-
   const handleClick = () => {
     count = count + 1;
     console.log(count);
   };
   return (
-    <div>
-      <h2>{count}</h2>
-      <button type='button' className='btn' onClick={handleClick}>
-        increment
+    <>
+      <h2>useState error example</h2>
+      <h2>{count} </h2>
+      <button type='button' onClick={handleClick} className='btn'>
+        Increase
       </button>
-    </div>
+    </>
   );
 };
 
