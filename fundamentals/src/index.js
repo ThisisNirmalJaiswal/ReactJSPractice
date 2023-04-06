7c12bcf3062efe19fb4cd46e242cf126aa91d508
Nirmal Jaiswal <nirmaljaiswal7746@gmail.com>
2023-04-06 11:16:47 +0530

    props completed

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -2,68 +2,74 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const firstBook = {
-  author: 'Marilyn Sadler',
-  title:
-    "It's Not Easy Being a Bunny: An Easter Book for Kids and Toddlers(Beginner Books(R))",
-  img: './images/book.jpg',
-};
+const books = [
+  {
+    id: 1,
+    author: 'Jordan Moore',
+    title: 'Interesting Facts For Curious Minds',
+    img: './images/book-1.jpg',
+  },
+  {
+    id: 2,
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: 'https://m.media-amazon.com/images/I/513RN9tL-AL._SX329_BO1,204,203,200_.jpg',
+  },
+];
 
-const secondBook = {
-  author: 'James Clear',
-  title:
-    'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
-  img: 'https://m.media-amazon.com/images/I/513RN9tL-AL._SX329_BO1,204,203,200_.jpg',
-};
+// how to render a list of names or any data
 
+// const names = ['nirmal', 'ajay', 'vikash'];
+
+// const newNames = names.map((name) => {
+//   console.log(names);
+//   return <h1>{name}</h1>;
+// });
 const BookList = () => {
   return (
     <section className='booklist'>
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos in
-          veritatis beatae eos dignissimos necessitatibus quasi, error
-          aspernatur quibusdam quis. Ullam saepe voluptates laborum magni
-          repudiandae aliquam perferendis, doloribus modi!
-        </p>
-
-        <button>Click me</button>
-      </Book>
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
+      {books.map((book) => {
+        const { img, title, author, id } = book;
+        return <Book {...book} key={id} />;
+        {
+          /* return <Book book={book} key={id} />; */
+        }
+      })}
     </section>
   );
 };
 
-// // first method of using props by destructure
+// 1st option
 // const Book = (props) => {
-//   const { title, img, author } = props;
-//   console.log(props);
+//   const { img, title, author } = props.book;
 //   return (
 //     <article className='book'>
 //       <img src={img} alt={title} />
 //       <h2>{title}</h2>
-//       <h4>{author}</h4>
+//       <h2>{author}</h2>
 //     </article>
 //   );
 // };
 
-// second method of using props by destructure
-const Book = ({ title, img, author, children }) => {
-  //   const { title, img, author } = props;
+// 2nd option
+// const Book = ({ book: { img, title, author } }) => {
+//   return (
+//     <article className='book'>
+//       <img src={img} alt={title} />
+//       <h2>{title}</h2>
+//       <h2>{author}</h2>
+//     </article>
+//   );
+// };
+
+// 3rd option using spread operator
+const Book = (props) => {
+  const { img, title, author } = props;
   return (
     <article className='book'>
       <img src={img} alt={title} />
       <h2>{title}</h2>
-      <h4>{author}</h4>
-      {children}
+      <h2>{author}</h2>
     </article>
   );
 };
