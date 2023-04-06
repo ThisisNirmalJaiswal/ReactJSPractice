821d9332e05ab9fbdd84c566e84966763c030e13
Nirmal Jaiswal <nirmaljaiswal7746@gmail.com>
2023-04-06 09:51:38 +0530

    using value by variable instead of passing them directly

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+const author = 'Marilyn Sadler';
+const title =
+  "It's Not Easy Being a Bunny: An Easter Book for Kids and Toddlers(Beginner Books(R))";
+
+const img = './images/book.jpg';
 const BookList = () => {
   return (
     <section className='booklist'>
@@ -18,37 +23,22 @@ const BookList = () => {
 const Book = () => {
   return (
     <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+      {/* <p>{let p = 6 }</p> // it will doesn't work bcuz it not a value. */}
+      {/* <p>{6 + 6}</p> // it will work bcuz it's have some value. */}
     </article>
   );
 };
 
-const Image = () => {
-  return (
-    <img
-      src='./images/book.jpg'
-      alt="It's Not Easy Being a Bunny: An Easter Book for Kids and Toddlers (Beginner
-    Books(R))"
-    />
-  );
-};
-const Title = () => (
-  <h2>
-    It's Not Easy Being a Bunny: An Easter Book for Kids and Toddlers (Beginner
-    Books(R))
-  </h2>
-);
-const Author = () => {
-  const inlineHeadingsStyle = {
-    color: '#617d98',
-    fontSize: '0.75rem',
-    marginTop: '0.5rem',
-  };
-
-  return <h4 style={inlineHeadingsStyle}>Marilyn Sadler</h4>;
-};
+// const Author = () => {
+//   return (inlineHeadingsStyle = {
+//     color: '#617d98',
+//     fontSize: '0.75rem',
+//     marginTop: '0.5rem',
+//   });
+// };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
