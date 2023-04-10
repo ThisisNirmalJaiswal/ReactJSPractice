77cb9eedbd1d2145b9f890b983485ed6025fa035
Nirmal Jaiswal <nirmaljaiswal7746@gmail.com>
2023-04-10 16:46:12 +0530

    hook rule

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Final from './tutorial/03-conditional-rendering/final/02-multiple-returns-fetch-data';
+import Final from './tutorial/03-conditional-rendering/final/03-hooks-rule';
 
 function App() {
   return (
--- src/tutorial/03-conditional-rendering/final/03-hooks-rule.jsx
+++ src/tutorial/03-conditional-rendering/final/03-hooks-rule.jsx
@@ -1,20 +1,37 @@
+// import { useState, useEffect } from 'react';
+
+// const Example = () => {
+//   const [condition, setCondition] = useState(true);
+//   // if (condition) {
+//   //   // won't work
+//   //   const [state, setState] = useState(false);
+//   // }
+
+//   if (condition) {
+//     return <h2>Hello There</h2>;
+//   }
+//   // this will also fail
+//   useEffect(() => {
+//     console.log('hello there');
+//   }, []);
+//   return <h2>example</h2>;
+// };
+
+// export default Example;
+
 import { useState, useEffect } from 'react';
 
 const Example = () => {
   const [condition, setCondition] = useState(true);
-  // if (condition) {
-  //   // won't work
-  //   const [state, setState] = useState(false);
-  // }
-
   if (condition) {
-    return <h2>Hello There</h2>;
+    return <h1>Hello There</h1>;
   }
-  // this will also fail
+
   useEffect(() => {
     console.log('hello there');
-  }, []);
-  return <h2>example</h2>;
+  }, [condition]);
+
+  return <h1>Example</h1>;
 };
 
 export default Example;
