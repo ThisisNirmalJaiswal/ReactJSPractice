e16cfd633e32856c40ee8a5df94c761477fdd0ec
Nirmal Jasval <nirmaljaiswal7746@gmail.com>
2023-04-13 09:19:02 +0530

    custom hooks2

--- src/tutorial/08-custom-hooks/final/01-toggle.jsx
+++ src/tutorial/08-custom-hooks/final/01-toggle.jsx
@@ -1,7 +1,7 @@
 import useToggle from './useToggle';
 
 const ToggleExample = () => {
-  const { show, toggle } = useToggle(true);
+  const [show, toggle] = useToggle(false);
   return (
     <div>
       <h4>toggle custom hook</h4>
