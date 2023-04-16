0e73593c740dd28bbd6e87d1d2bee78f2bf100e0
Nirmal Jasval <nirmaljaiswal7746@gmail.com>
2023-04-16 11:58:23 +0530

    react 18 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Starter from './tutorial/09-context-api/starter/context/Navbar';
-import Final from './tutorial/09-context-api/final/context/Navbar';
+import Starter from './tutorial/11-performance/starter/04-react-18/index';
+import Final from './tutorial/11-performance/final/04-react-18/index';
 
 function App() {
   return (
-    <div>
-      <Final />
+    <div className='container'>
+      {/* <Final /> */}
       <Starter />
     </div>
   );
--- src/tutorial/02-useEffect/final/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/final/04-fetch-data.jsx
@@ -38,24 +38,25 @@
 // };
 // export default FetchData;
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 const url = 'https://api.github.com/users';
 
 const FetchData = () => {
   const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(url);
-        const users = await response.json();
-        setUsers(users);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchData();
+  const fetchData = useCallback(async () => {
+    try {
+      const response = await fetch(url);
+      const users = await response.json();
+      setUsers(users);
+    } catch (err) {
+      console.log(err);
+    }
   }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
   return (
     <section>
